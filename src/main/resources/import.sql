18cd040f0de48b95dbb821ca3e0fd85919a842b6
Erxck-dev <erick.valladares6223@utc.edu.ec>
2026-01-19 18:24:40 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,3 @@
-INSERT INTO rol (nombre) VALUES ('ROLE_USER') ON CONFLICT DO NOTHING;
-INSERT INTO rol (nombre) VALUES ('ROLE_ADMIN') ON CONFLICT DO NOTHING;
-
--- 2. Convertir tu usuario en ADMIN
--- Reemplaza 'tu_correo@gmail.com' por el correo con el que hiciste el registro en la web
 INSERT INTO usuarios_roles (usuario_id, rol_id)
 SELECT u.id, r.id 
 FROM usuarios u, rol r 
